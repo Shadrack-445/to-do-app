c87d9ebbdefec02d0fa75d8a1396d584c4831f8b
Shadrack-445 <shadrackomondi17@gmail.com>
2025-06-23 16:41:54 +0300

    first commit

---
+++ src/Todo.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+function Todo() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTask = () => {
+    setTasks([...tasks, { id, text: input, done: false }]);
+    setInput('');
+  };
+
+  const toggleTask = (id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, done: !task.done } : task
+    ));
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const editTask = (id) => {
+    const newText = prompt("Edit task:");
+    if (newText !== null) {
+      setTasks(tasks.map(task =>
+        task.id === id ? { ...task, text: newText } : task
+      ));
+    }
+  };
+
+  return (
+    <div>
+      <h2>To-Do</h2>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add task"
+      />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            <span
+              onClick={() => toggleTask(task.id)}
+              style={{
+                textDecoration: task.done ? 'line-through' : 'none',
+              }}
+            >
+              {task.text}
+            </span>
+            <button onClick={() => editTask(task.id)}>Edit</button>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+
+}
+export default Todo
